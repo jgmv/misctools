6649717219556eabc9d5988104ff55e89f8ef066
jose@t15g2 <jg.macia@gmail.com>
2024-03-20 15:10:06 +0100

    find_species_range() added

---
+++ R/find_species_range.R
@@ -0,0 +1,23 @@
+#' Find species distribution range
+#'
+#' Calculates the range of a species, in Km.
+#' @param x data frame with the coordinates of the species occurrence.
+#' @param lon name of data frame column with the longitude data.
+#' @param lat of data frame column with the latitude data.
+#' @keywords distance
+#' @export
+#' @examples
+#' x <- data.frame(longitude = runif(10, min = -180, max = 180),
+#'   latitude = runif(10, min = -90, max = 90))
+#' find_species_range(x)
+find_species_range <- function(x, lon = "longitude", lat = "latitude") {
+  xy <- as.matrix(x[, c(lon, lat)])
+  xy <- stats::na.omit(xy)
+  if(nrow(xy) > 0) {
+    xy_dist <- sp::spDists(xy, longlat = T)
+    result <- max(xy_dist)
+  } else {
+    result <- NA
+  }
+  return(result)  
+}
